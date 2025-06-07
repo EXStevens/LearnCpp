f16476ee554ecf2217b611fd6b617a162ca64913
Charlie Stevens <cstevens@expcs.net>
2025-06-07 13:23:46 +0800

    correct companion filename in twofile comment

--- CppPrimerPlus/Chapter 9/twofile1.cpp
+++ CppPrimerPlus/Chapter 9/twofile1.cpp
@@ -1,5 +1,5 @@
 // twofile1.cpp -- variables with external and internal linkage
-#include <iostream>     // to be compiled with two file2.cpp
+#include <iostream>     // to be compiled with twofile2.cpp
 int tom = 3;            // external variable definition
 int dick = 30;          // external variable definition
 static int harry = 300; // static, internal linkage
